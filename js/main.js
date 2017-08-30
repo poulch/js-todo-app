ca949e1e95fc5f628dce13b4759d6c793481f12b
Paweł Chyła <chyla1988@gmail.com>
2017-08-30 21:24:54 +0200

    Simplification todo class

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const todoApp = new todoClass();
-  todoApp.init('.todo-container');
-});
+(function() {
+
+  const todoApp = new Todo();  
+
+  function init(container) {
+    const todoContainer = document.querySelector(container);
+    const unorderList = document.createElement('ul');
+    const restoreBtn =todoApp.createRestoreBtn();
+    
+
+    unorderList.classList.add('list');
+    todoContainer.appendChild(unorderList);
+    todoApp.list = document.querySelector('.list');
+    
+
+
+    const listItemsFragment = document.createDocumentFragment();
+    const createLI = todoApp.createListItem.bind(todoApp);
+    todoApp.todoList.forEach(function (element) {
+      listItemsFragment.appendChild(createLI(element));
+    });
+    todoApp.list.appendChild(listItemsFragment);
+
+    document.querySelector('.todo-container').appendChild(restoreBtn);
+    todoApp.restoreBtn = document.querySelector('.restore-btn');
+    todoApp.restoreBtn.classList.add('hidden');
+    todoApp.restoreBtn.addEventListener('click', todoApp.restoreLastItem);
+
+    if (todoApp.lastRemoved.length > 0) {
+      todoApp.restoreBtn.classList.remove('hidden');
+    }
+
+    todoApp.form.addEventListener('submit', todoApp.formSubmit);
+    todoApp.list.addEventListener('click', todoApp.removeElementFromDOM);
+  }
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    init('.todo-container');
+  });
+})();
--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,12 @@
-class todoClass {
+class Todo {
+
   constructor() {
     this.todoList = this.fetchList();
     this.lastRemoved = this.fetchRemoved();
-    this.list = document.querySelector('.list');
     this.form = document.querySelector('.form-container form');
+    this.list = document.querySelector('.list');
     this.input = document.querySelector('.form-container .form-input');
     this.restoreBtn;
-
     this.formSubmit = this.formSubmit.bind(this);
     this.removeElementFromDOM = this.removeElementFromDOM.bind(this);
     this.restoreLastItem = this.restoreLastItem.bind(this);
@@ -16,6 +16,7 @@ class todoClass {
     this.todoList.push(item);
     this.saveList(this.todoList);
   }
+
   removeItem(value) {
     const indexRemovedElement = this.todoList.indexOf(value);
     const removedElement = this.todoList.splice(indexRemovedElement, 1);
@@ -28,31 +29,32 @@ class todoClass {
       if (this.restoreBtn.classList.contains('hidden')) {
         this.restoreBtn.classList.remove('hidden');
       }
-
     }
   }
+
   fetchList() {
-    let todoArray = [];
-    const todo_str = localStorage.getItem('todo');
-    todo_str ? todoArray = JSON.parse(todo_str) : [];
-    return todoArray;
+    const todoString = localStorage.getItem('todo');
+    return todoString ? JSON.parse(todoString) : [];
   }
-  fetchRemoved() {
-    let last = [];
-    const lastString = localStorage.getItem('last');
-    lastString ? last = JSON.parse(lastString) : [];
-    if (last.length > 2) {
-      last.length = 2;
-    }
 
-    return last;
+  fetchRemoved() {
+    let lastRemoved = [];
+    const lastString = localStorage.getItem('last');
+    lastString ? lastRemoved = JSON.parse(lastString) : [];
+    if (lastRemoved.length > 2) {
+      lastRemoved.length = 2;
+    }
+    return lastRemoved;
   }
+
   saveList(list) {
     localStorage.setItem('todo', JSON.stringify(list));
   }
+
   saveRemoved(array) {
     localStorage.setItem('last', JSON.stringify(array));
   }
+
   restoreLastItem(element) {
     if (this.lastRemoved.length > 0) {
       const removedElement = this.lastRemoved.splice(0, 1);
@@ -64,6 +66,7 @@ class todoClass {
       element.target.classList.add('hidden');
     }
   }
+
   createListItem(value) {
     const listItem = document.createElement('li');
     const cancelBtn = document.createElement('div');
@@ -80,6 +83,7 @@ class todoClass {
 
     return listItem;
   }
+
   createRestoreBtn() {
     const restoreContainer = document.createElement('div');
     const restoreBtn = document.createElement('button');
@@ -90,6 +94,7 @@ class todoClass {
     restoreContainer.appendChild(restoreBtn);
     return restoreContainer;
   }
+
   removeElementFromDOM(element) {
     const target = element.target;
     let parent = null;
@@ -103,40 +108,13 @@ class todoClass {
       this.removeItem(parent.textContent);
     }
   }
+
   formSubmit(e) {
     e.preventDefault();
+    const input = document.querySelector('.form-container .form-input');
     const value = this.input.value;
     this.input.value = '';
     this.list.appendChild(this.createListItem(value));
     this.addItem(value);
-  }
-  init(container) {
-    const todoContainer = document.querySelector(container);
-    const unorderList = document.createElement('ul');
-    const restoreBtn = this.createRestoreBtn();
-
-    unorderList.classList.add('list');
-    todoContainer.appendChild(unorderList);
-    this.list = unorderList;
-
-
-    const listItemsFragment = document.createDocumentFragment();
-    const createLI = this.createListItem.bind(this);
-    this.todoList.forEach(function (element) {
-      listItemsFragment.appendChild(createLI(element));
-    });
-    this.list.appendChild(listItemsFragment);
-
-    document.querySelector('.todo-container').appendChild(restoreBtn);
-    this.restoreBtn = document.querySelector('.restore-btn');
-    this.restoreBtn.classList.add('hidden');
-    this.restoreBtn.addEventListener('click', this.restoreLastItem);
-
-    if (this.lastRemoved.length > 0) {
-      this.restoreBtn.classList.remove('hidden');
-    }
-
-    this.form.addEventListener('submit', this.formSubmit);
-    this.list.addEventListener('click', this.removeElementFromDOM);
   }
 }
